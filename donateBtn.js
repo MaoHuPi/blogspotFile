f12f28c48c225396533087162e4308baed5e076d
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2022-01-13 23:47:08 +0800

    Create donateBtn.js

---
+++ donateBtn.js
@@ -0,0 +1,30 @@
+(() => {
+    let data = [
+        {
+            'text':'Ko-fi', 
+            'color':'#86c3ee', 
+            'image':'https://storage.ko-fi.com/cdn/cup-border.png', 
+            'link':'https://ko-fi.com/maohupi'
+        }, 
+        {
+            'text':'Liker.Land', 
+            'color':'#56b893', 
+            'image':'', 
+            'link':'https://liker.land/jhou5846/civic'
+        }
+    ], 
+    div = document.createElement('div');
+    data.forEach(index => {
+        let button = document.createElement('button'), 
+        img = document.createElement('div'), 
+        text = document.createElement('span');
+        // img
+        img.src = index['image'];
+        button.appendChild(img);
+        // text
+        text.innerText = index['text'];
+        button.appendChild(text);
+        // button
+        div.appendChild(button);
+    });
+})(); 
