7d057e3525b91f30b9ffd937cf930e1fd46e88fe
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2022-01-18 22:05:11 +0800

    Create pushSettings.js

---
+++ pushSettings.js
@@ -0,0 +1,4 @@
+// icon
+document.head.querySelectorAll('link[rel*="icon"]').forEach(E => {
+  E.href = 'https://blogger.googleusercontent.com/img/a/AVvXsEgLOsCYQvs3Wn9Ms1GiCzWBdYOibSTHOH2whDTAabNVDSefXFLeDT6w1PK7G87ywVBH8JuNWMmSf5QFcac6R9VAAmCGZ3vqg1xsnEx0MdXg_lZWt30bSUSvuyfUC812DdeWM2M8rWPjyfeLRxdr2m9v_rfyb12jNirA2sNKNgZlxxVpoe3x1o5-UO7V';
+});
