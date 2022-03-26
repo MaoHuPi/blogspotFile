1ed19bc1d0a11aa49ee2554aec57d8538781eeee
鈦匕工作室 - 貓虎皮 <60348735+MaoHuPi@users.noreply.github.com>
2022-03-27 07:23:09 +0800

    Update likeBtn.js

--- likeBtn.js
+++ likeBtn.js
@@ -2,7 +2,7 @@
   let likeBtn = document.querySelector('#likeBtn'), 
   iframe = document.createElement('iframe');
   iframe.src = `https://button.like.co/in/embed/jhou5846/button/?referrer=${location.href}`;
-  iframe.style.margin = 'auto';
+  iframe.style.width= '50%';
   iframe.setAttribute('scrolling', 'no');
   iframe.setAttribute('frameborder', '0');
   iframe.setAttribute('data-ruffle-polyfilled', '');
